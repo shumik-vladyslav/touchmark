c8b1455ecb9b79cb79590bb371797f4aa47d96c2
Vladyslav Shumik <vladyslav.shumik@adrout.net>
2016-03-24 14:22:02 +0200

    layout of the main page

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,7 +6,7 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig) {
+  function config($logProvider,$mdThemingProvider, toastrConfig) {
     // Enable log
     $logProvider.debugEnabled(true);
 
@@ -16,6 +16,28 @@
     toastrConfig.positionClass = 'toast-top-right';
     toastrConfig.preventDuplicates = true;
     toastrConfig.progressBar = true;
-  }
+    
+
+    $mdThemingProvider.definePalette('white', {
+        '50': 'ffffff',
+        '100': 'ffffff',
+        '200': 'ffffff',
+        '300': 'ffffff',
+        '400': 'ffffff',
+        '500': 'ffffff',
+        '600': 'ffffff',
+        '700': 'ffffff',
+        '800': 'ffffff',
+        '900': 'ffffff',
+        'A100': 'ffffff',
+        'A200': 'ffffff',
+        'A400': 'ffffff',
+        'A700': 'ffffff',
+        'contrastDefaultColor': 'dark'
+    });
+
+    $mdThemingProvider.theme('default')
+        .primaryPalette('white');
+    }
 
 })();
